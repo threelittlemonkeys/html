c60a19a67c0c796565311e6c7bf41b0e80c66a53
threelittlemonkeys <sjlee7642@gmail.com>
2020-05-25 18:23:34 +0900

    Update by commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
-window.onresize = function() {
-    console.log(document.documentElement.clientHeight);
-};
+window.addEventListener("load", resize);
+window.addEventListener("resize", resize);
+
+function resize() {
+    e = document.getElementById("sidebar")
+    if (e.offsetTop < 80) {
+        e.style.top = "5em";
+        e.style.bottom = "";
+        console.log(e.offsetTop);
+    } else {
+        e.style.top = "";
+        e.style.bottom = "2em";
+    }
+}
 
 // http://www.nextree.co.kr/p4150/
