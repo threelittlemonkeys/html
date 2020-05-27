4af2d87e675df3250d43497abe2a54afac2f23e6
threelittlemonkeys <sjlee7642@gmail.com>
2020-05-27 12:51:28 +0900

    Update by commit

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ window.addEventListener("resize", sidebar_reposition);
 
 function sidebar_reposition() {
     e = document.getElementById("sidebar");
+    // 3 + 2 + 2 = 7em = 112px
     if (document.body.clientHeight < e.clientHeight + 112) {
         e.style.top = "5em";
         e.style.bottom = "";
@@ -10,6 +11,11 @@ function sidebar_reposition() {
         e.style.top = "";
         e.style.bottom = "2em";
     }
+}
+
+function model1(e) {
+    txt = e.innerHTML;
+    console.log(txt);
 }
 
 // http://www.nextree.co.kr/p4150/
