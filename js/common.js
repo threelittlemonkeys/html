1c45643bb6428268c7dac8800d6349888119da03
threelittlemonkeys <sjlee7642@gmail.com>
2020-05-27 16:59:20 +0900

    Update by commit

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ window.addEventListener("load", sidebar_reposition);
 window.addEventListener("resize", sidebar_reposition);
 
 function sidebar_reposition() {
-    e = document.getElementById("sidebar");
+    var e = document.getElementById("sidebar");
     // 3 + 2 + 2 = 7em = 112px
     if (document.body.clientHeight < e.clientHeight + 112) {
         e.style.top = "5em";
@@ -14,8 +14,23 @@ function sidebar_reposition() {
 }
 
 function model1(e) {
-    txt = e.innerHTML;
-    console.log(txt);
+    var x = document.createElement("span");
+    x.id = "test";
+    window.getSelection().getRangeAt(0).insertNode(x);
+    e.blur();
+    e.innerHTML = e.innerHTML.replace("abc", "def");
+    e.focus();
+    var range = document.createRange();
+    x = document.getElementById("test");
+    range.selectNode(x);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    range.deleteContents();
+    console.log(e.innerHTML);
 }
 
-// http://www.nextree.co.kr/p4150/
+/*
+http://www.nextree.co.kr/p4150/
+https://m.blog.naver.com/PostView.nhn?blogId=writer0713&logNo=220499119278&proxyReferer=https:%2F%2Fwww.google.com%2F
+*/
