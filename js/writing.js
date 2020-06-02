3bd268ecdd0276f816fe9a2852acc8bd131de7df
threelittlemonkeys <sjlee7642@gmail.com>
2020-06-02 16:05:35 +0900

    Update by commit

--- js/writing.js
+++ js/writing.js
@@ -1,11 +1,10 @@
-var CARRET_POSITION = -1;
+var PAGE_TEXT = "";
 
 aEL(window, "load", sidebar_reposition);
 aEL(window, "resize", sidebar_reposition);
 
-aEL(document, "DOMContentLoaded", function() {
-    aEL(gEBI("page_text"), "click", model1);
-    aEL(gEBI("page_text"), "keyup", model1);
+aEL(document, "DOMContentLoaded", () => {
+    setInterval(() => model1(gEBI("page_text")), 1000);
 });
 
 function sidebar_reposition() {
@@ -20,26 +19,26 @@ function sidebar_reposition() {
     }
 }
 
-function model1(el, ev) {
-    p = window.getSelection().focusOffset;
-    if (CARRET_POSITION >= 0 && p == CARRET_POSITION)
+function model1(e) {
+    if (PAGE_TEXT == e.innerHTML)
         return;
-    CARRET_POSITION = p;
+    p = window.getSelection().focusOffset - 1;
+    w = e.innerText.charAt(p);
+    return;
+    PAGE_TEXT = e.innerHTML;
     console.log(p);
+    console.log(e.innerText.charAt(p));
     return;
-    if (PAGE_TEXT == el.innerHTML)
-        return;
-    console.log(ev);
-    return;
-    var x = document.createElement("span");
-    x.id = "test";
-    window.getSelection().getRangeAt(0).insertNode(x);
+
+    var cc = document.createElement("span");
+    cc.id = "test";
+    window.getSelection().getRangeAt(0).insertNode(cc);
     e.blur();
-    e.innerHTML = e.innerHTML.replace("abc", "def");
+    e.innerHTML = e.innerHTML.replace("abc", "xxx");
     e.focus();
     var range = document.createRange();
-    x = gEBI("test");
-    range.selectNode(x);
+    cc = gEBI("test");
+    range.selectNode(cc);
     var selection = window.getSelection();
     selection.removeAllRanges();
     selection.addRange(range);
